f0bfeb70412f300950218e2d0e057d3b47c0c205
Maofryy <41508644+Maofryy@users.noreply.github.com>
2021-04-26 11:20:26 +0200

    Manual testing needed on local machine

--- cli_port_sniffer/src/main.rs
+++ cli_port_sniffer/src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
-use std::net::IpAddr;
-use std::str::FromStr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::process;
+use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
-const MAX_THREADS: u16 = 65535;
+const MAX_THREADS: u16 = 1024;
 
 struct Arguments {
     flag: String,
@@ -20,7 +23,11 @@ impl Arguments {
         }
         let f = args[1].clone();
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
-            return Ok(Arguments{flag: String::from(""), ipaddr, threads: 4});
+            return Ok(Arguments {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
         } else {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("--help") && args.len() == 2 {
@@ -37,12 +44,34 @@ impl Arguments {
                     Ok(s) => s,
                     Err(_) => return Err("Incorrect number format."),
                 };
-                return Ok(Arguments{flag, ipaddr, threads});
+                return Ok(Arguments {
+                    flag,
+                    ipaddr,
+                    threads,
+                });
             } else {
                 return Err("Invalid syntax.");
             }
         }
-        
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX_THREADS - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
     }
 }
 
@@ -59,16 +88,37 @@ fn main() {
     // Handle args
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let arguments = Arguments::new(&args).unwrap_or_else(
-        |err| {
-            if err.contains("help") {
-                process::exit(0);
-            } else {
-                eprintln!("{} problem parsing arguments: {}", program, err);
-                process::exit(1);
-            }
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(1);
         }
-    );
+    });
+    // divide into threadd, scan ip
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
 
 #[cfg(test)]
@@ -79,14 +129,23 @@ mod tests {
     #[test]
     fn test_args() {
         assert!(Arguments::new(&["./test".to_string(), "-h".to_string()]).is_err());
-        assert!(Arguments::new(&["./test".to_string(),"-j".to_string(), "15".to_string(), "192.168.1.52".to_string()]).is_ok());
-        assert!(Arguments::new(&["./test".to_string(),"192.168.1.52".to_string()]).is_ok());
-        assert!(Arguments::new(&["-j".to_string(), "nb".to_string(), "192.168.1.52".to_string()]).is_err());
+        assert!(Arguments::new(&[
+            "./test".to_string(),
+            "-j".to_string(),
+            "15".to_string(),
+            "192.168.1.52".to_string()
+        ])
+        .is_ok());
+        assert!(Arguments::new(&["./test".to_string(), "192.168.1.52".to_string()]).is_ok());
+        assert!(Arguments::new(&[
+            "-j".to_string(),
+            "nb".to_string(),
+            "192.168.1.52".to_string()
+        ])
+        .is_err());
         assert!(Arguments::new(&["./test".to_string(), "1192.168.1.52".to_string()]).is_err());
     }
 
     #[test]
-    fn test_scan() {
-
-    }
+    fn test_scan() {}
 }
